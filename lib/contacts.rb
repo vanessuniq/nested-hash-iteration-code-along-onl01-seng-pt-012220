e63d217aa4562c95a7b4574eb65bcaa3b101823e
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-03 05:15:10 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, data|
+    if person == "Freddy Mercury"
+      data.each do |attribute, value|
+        if attribute == :favorite_ice_cream_flavors
+          value.delete_if {|ice_cream| ice_cream == "strawberry" }
+        end
+      end
+    end 
+  end 
 end
